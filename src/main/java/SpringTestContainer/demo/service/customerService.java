1ac294a83f818db2ccf21ec44ceb054fb7799aa4
Gnanesh26 <gnaneswarrao.yeddu@imaginnovate.com>
2024-04-30 14:25:24 +0530

    added test cases for post,delete,put api's

--- src/main/java/SpringTestContainer/demo/service/customerService.java
+++ src/main/java/SpringTestContainer/demo/service/customerService.java
@@ -13,8 +13,23 @@ public class customerService {
     @Autowired
     CustomerRepository customerRepository ;
 
+    public Customer addCustomer(Customer customer) {
+        return customerRepository.save(customer);
+    }
+
     public Customer getCustomerById(Long id) {
         return customerRepository.findById(id).orElse(null);
     }
 
+    public Customer updateCustomer(Long id, Customer customer) {
+        if (customerRepository.existsById(id)) {
+            customer.setId(id);
+            return customerRepository.save(customer);
+        }
+        return null;
+    }
+
+    public void deleteCustomer(Long id) {
+        customerRepository.deleteById(id);
+    }
 }
